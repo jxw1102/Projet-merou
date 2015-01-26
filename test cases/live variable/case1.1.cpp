dfbdaa70099e08cf48c94545532a66678d8bafbe
Ji Xiaowen <xiaowen.ji@me.com>
2015-01-26 12:57:18 +0100

    test case 1

---
+++ test cases/live variable/case1.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+	int x = 50;
+	int y = 10;
+	if (y > 5)
+		x = 9;
+	else
+		x = 8;
+    std::cout << x << std::endl;
+	return 0;
+}
+
+// x : {4}
+// y : null
---
+++ test cases/live variable/case1.3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int argc, char** argv) {
+    int x = 50;
+    int y = 10;
+    while (y > 5) {
+        x++;
+        y--;
+    }
+    std::cout << x << std::endl;
+    return 0;
+}
+
+// x : null
+// y : null
