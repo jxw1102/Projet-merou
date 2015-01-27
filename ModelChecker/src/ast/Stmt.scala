752d80bf505daf94c9127d9cbbf344b61313bf25
Dicee <courtinot.david@orange.fr>
2015-01-27 16:53:06 +0100

    Some statements for the members to compile their code

--- ModelChecker/src/ast/Stmt.scala
+++ ModelChecker/src/ast/Stmt.scala
@@ -3,3 +3,6 @@ package ast
 class Stmt {
 
 }
+
+abstract class CompoundStmt(var body: List[Stmt])
+class IfStmt(val condition: Expr, val body: CompoundStmt, val elseStmt: Option[CompoundStmt]) extends Stmt
