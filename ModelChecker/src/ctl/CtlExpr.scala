6f867508c54fdd83083806edc279fd5aaddc7792
simple <simple@localhost.localdomain>
2015-01-27 17:46:54 +0100

    CTL add free class

--- ModelChecker/src/ctl/CtlExpr.scala
+++ ModelChecker/src/ctl/CtlExpr.scala
@@ -1,5 +1,21 @@
 package ctl
 
-class CtlExpr {
+abstract class CtlExpr 
 
-}
+// Binary Expression
+case class And (left : CtlExpr, right : CtlExpr)
+case class Or  (left : CtlExpr, right : CtlExpr)
+case class AU  (left : CtlExpr, right : CtlExpr)
+case class EU  (left : CtlExpr, right : CtlExpr)
+
+// Unary Expression
+case class AX  (right : CtlExpr)
+case class EX  (right : CtlExpr)
+case class AG  (right : CtlExpr)
+case class EG  (right : CtlExpr)
+case class AF  (right : CtlExpr)
+case class EF  (right : CtlExpr)
+case class Not (right : CtlExpr)
+
+// 
+
