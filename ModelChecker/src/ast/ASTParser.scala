356d6b026a1602e8a5ef1603f4fec9a27d59c640
Ji Xiaowen <xiaowen.ji@me.com>
2015-01-28 18:41:16 +0100

    ASTNode Tree

--- ModelChecker/src/ast/ASTParser.scala
+++ ModelChecker/src/ast/ASTParser.scala
@@ -38,13 +38,13 @@ object ASTParser {
     	val idReg = "(\\w+) (0x[\\da-f]{9})".r
         
         var stack = ArrayStack[ASTNode]()
-        var head = OtherASTNode(0, "")
-        stack.push(head)
+        var tree = OtherASTNode(0, "")
+        stack.push(tree)
     	
     	lines.takeWhile(_.indent >= indent).foreach(line => {
     		val idMatcher = idReg.findFirstMatchIn(line)
             val data = line.substring(line.indent)
-    		val node = idMatcher match {
+    		val node: ASTNode = idMatcher match {
     		    case Some(m) =>
     				ConcreteASTNode(line.indent/2,m.group(1),java.lang.Long.parseLong(m.group(2).substring(2),16),getCodeRange(line).get,data)
     		    case None    =>
@@ -52,22 +52,34 @@ object ASTParser {
                         case "<<<NULL>>>"    =>    NullASTNode(line.indent/2)
                         case _               =>    OtherASTNode(line.indent/2,data)
                     }
-    		}
-            
+            }
+            while(node.depth <= stack.head.depth) {
+                stack.pop()
+            }
+            stack.head.children += node
+            stack.push(node)
     	})
         
+        def displayChildren(t: ASTNode) : Unit = {
+            for(n <- t.children) {
+                println("  " * n.depth + n)
+                displayChildren(n)
+            }
+        }
+        
+        println(tree)
+        displayChildren(tree)
         
     }
 }
 
-abstract class ASTNode(depth: Int)
-case class ConcreteASTNode(depth: Int, ofType: String, id: Long, pos: CodeRange, data: String) extends ASTNode(depth) {
+abstract class ASTNode(_depth: Int) {
+    def depth = _depth
     val children = ArrayBuffer[ASTNode]()
 }
-case class NullASTNode(depth: Int) extends ASTNode(depth)
-case class OtherASTNode(depth:Int, data: String) extends ASTNode(depth) {
-    val children = ArrayBuffer[ASTNode]()
-}
+case class ConcreteASTNode(_depth: Int, ofType: String, id: Long, pos: CodeRange, data: String) extends ASTNode(_depth)
+case class NullASTNode(_depth: Int) extends ASTNode(_depth)
+case class OtherASTNode(_depth:Int, data: String) extends ASTNode(_depth)
 
 case class CodeRange(lineMin: Int, lineMax: Int, colMin: Int, colMax: Int) {
     val lineRange = lineMin to lineMax
