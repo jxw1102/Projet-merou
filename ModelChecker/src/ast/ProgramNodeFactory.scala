c9c3fc15b13e11d326dbd7c8d07aed24e8b6c920
Sofia Boutahar <sofiaboutahar@Sofias-MacBook-Pro.local>
2015-02-03 16:31:42 +0100

    d

--- ModelChecker/src/ast/ProgramNodeFactory.scala
+++ ModelChecker/src/ast/ProgramNodeFactory.scala
@@ -1,25 +1,48 @@
 package ast
 
 import cfg.GraphNode
+import scala.collection.mutable.Set
 
 object ProgramNodeFactory {
     type GNode = GraphNode[ProgramNode,ProgramNodeLabelizer]
+    type MHSet = scala.collection.mutable.HashSet[GNode]
 	def handleSourceCodeNode(node: SourceCodeNode): (GNode,Set[GNode]) = node match {
-	    case IfStmt(condition,body,elseStmt) => handleIf(node.asInstanceOf[IfStmt])
-	        
+	    case IfStmt(condition,body,elseStmt)   => handleIf(node.asInstanceOf[IfStmt])
+	    case ForStmt(init, cond, update, body) => handleFor(node.asInstanceOf[ForStmt])
+        case WhileStmt(condition, body)        => handleWhile(node.asInstanceOf[WhileStmt])
+        case DoWhileStmt(condition, body)      => handleDoWhile(node.asInstanceOf[DoWhileStmt])
+        
 	    
 	}
 
 	def handleIf(ifStmt: IfStmt) = ifStmt match {
-	    case IfStmt(condition,body,elseStmt) => 
-	        val res   = new GNode(If(condition))
-	        val left  = handleSourceCodeNode(body) 
-	        
-//	        val 
-	        res.addNext(left)
-	        if (elseStmt.isDefined) res.addNext(handleSourceCodeNode(elseStmt.get))
-	        res
-	}
+        case IfStmt(condition,body,elseStmt) => 
+            val res   = new GNode(If(condition))
+            val left  = handleSourceCodeNode(body) 
+            
+            val out   = new MHSet()
+            out     ++= left._2 
+            res.addNext(left._1)
+            elseStmt match {
+                case Some(x) => 
+                    val right = handleSourceCodeNode(x)
+                    out     ++= right._2
+                    res.addNext(right._1)
+                case None    =>
+            }
+            (res,out)
+    }
 	
-	def handleWhile()
+	def handleWhile(whileStmt: WhileStmt){
+        
+    }
+    
+    def handleDoWhile(doWhileStmt: DoWhileStmt){
+        
+    }
+    
+    def handleFor(whileStmt: WhileStmt){
+        
+    }
+    
 }
